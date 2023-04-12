0ad2b89c6d3ebb5393327ab6f1b7e46a9fab16fb
middlepenguin <middlepenguin@yandex.ru>
2023-04-12 19:36:51 +0300

    changed textures added matrix

--- src/Renderer/ShaderProgram.cpp
+++ src/Renderer/ShaderProgram.cpp
@@ -1,5 +1,7 @@
 #include "ShaderProgram.h"
 
+#include <glm/gtc/type_ptr.hpp>
+
 #include <iostream>
 
 namespace Renderer {
@@ -87,5 +89,9 @@ namespace Renderer {
 	{
 		glUniform1i(glGetUniformLocation(m_ID, name.c_str()), value);
 
+	}
+	void ShaderProgram::setMatrix4(const std::string& name, const glm::mat4& matrix)
+	{
+		glUniformMatrix4fv(glGetUniformLocation(m_ID, name.c_str()), 1, GL_FALSE, glm::value_ptr(matrix));
 	}
 }
