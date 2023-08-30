ee1d90b567da9e4fe9a240f00266f5989d8bac02
middlepenguin <middlepenguin@yandex.ru>
2023-08-30 18:31:27 +0300

    Animated Sprites added

--- src/Resources/ResourceManager.h
+++ src/Resources/ResourceManager.h
@@ -10,6 +10,7 @@ namespace Renderer
     class ShaderProgram;
     class Texture2D;
     class Sprite;
+    class AnimatedSprite;
 }
 
 
@@ -27,7 +28,7 @@ public:
     std::shared_ptr<Renderer::ShaderProgram> getShaderProgram(const std::string& shaderName);
     std::shared_ptr<Renderer::Texture2D> loadTexture(const std::string& textureName, const std::string& texturePath);
     std::shared_ptr<Renderer::Texture2D> getTexture(const std::string& textureName);
-
+    /// norm:
     std::shared_ptr<Renderer::Sprite> loadSprite(const std::string& spriteName,
         const std::string& textureName,
         const std::string& shaderName,
@@ -35,12 +36,20 @@ public:
         const unsigned int spriteHeight,
         const std::string& subTextureName = "default");
     std::shared_ptr<Renderer::Sprite> getSprite(const std::string& spriteName);
+    /// animated:
+    std::shared_ptr<Renderer::AnimatedSprite> loadAnimatedSprite(const std::string& spriteName,
+        const std::string& textureName,
+        const std::string& shaderName,
+        const unsigned int spriteWidth,
+        const unsigned int spriteHeight,
+        const std::string& subTextureName = "default");
+    std::shared_ptr<Renderer::AnimatedSprite> getAnimatedSprite(const std::string& spriteName);
 
-    std::shared_ptr<Renderer::Texture2D> loatTextureAtlas(const std::string textureName,
-        const std::string texturePath,
-        const std::vector<std::string> subTextures,
-        const unsigned int subTextureWidth,
-        const unsigned int subTextureHeight);
+    std::shared_ptr<Renderer::Texture2D> loatTextureAtlas(std::string textureName,
+                                                          std::string texturePath,
+                                                          std::vector<std::string> subTextures,
+                                                          const unsigned int subTextureWidth,
+                                                          const unsigned int subTextureHeight);
 
 private:
     std::string getFileString(const std::string& relativeFilePath) const;
@@ -54,5 +63,7 @@ private:
     typedef std::map<const std::string, std::shared_ptr<Renderer::Sprite>> SpritesMap;
     SpritesMap m_sprites;
 
+    typedef std::map<const std::string, std::shared_ptr<Renderer::AnimatedSprite>> AnimatedSpritesMap;
+    AnimatedSpritesMap m_animatedSprites;
     std::string m_path;
 };
