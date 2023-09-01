ce7ad7780e3e3ef951b2f48299a83cf63e7f51dd
middlepenguin <middlepenguin@yandex.ru>
2023-09-01 18:07:05 +0300

    New pngs

--- src/main.cpp
+++ src/main.cpp
@@ -40,7 +40,8 @@ void glfwWindowSizeCallback(GLFWwindow* pWindow, int width, int height)
     glViewport(0, 0, width, height);
 }
 
-void glfwKeyCallback(GLFWwindow* pWindow, int key, int scancode, int action, int mode)
+void glfwKeyCallback(GLFWwindow* pWindow, int key, int scancode,
+    int action, int mode)
 {
     if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
     {
@@ -62,7 +63,8 @@ int main(int argc, char** argv)
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     /* Create a windowed mode window and its OpenGL context */
-    GLFWwindow* pWindow = glfwCreateWindow(g_windowSize.x, g_windowSize.y, "Battle City", nullptr, nullptr);
+    GLFWwindow* pWindow = glfwCreateWindow(g_windowSize.x,
+        g_windowSize.y, "Battle City", nullptr, nullptr);
     if (!pWindow)
     {
         std::cout << "glfwCreateWindow failed!" << std::endl;
@@ -81,28 +83,37 @@ int main(int argc, char** argv)
         std::cout << "Can't load GLAD!" << std::endl;
     }
 
-    std::cout << "Renderer: " << glGetString(GL_RENDERER) << std::endl;
-    std::cout << "OpenGL version: " << glGetString(GL_VERSION) << std::endl;
+    std::cout << "Renderer: " <<
+        glGetString(GL_RENDERER) << std::endl;
+    std::cout << "OpenGL version: " 
+        << glGetString(GL_VERSION) << std::endl;
 
     glClearColor(1, 1, 0, 1);
 
     {
         ResourceManager resourceManager(argv[0]);
-        auto pDefaultShaderProgram = resourceManager.loadShaders("DefaultShader", "res/shaders/vertex.txt", "res/shaders/fragment.txt");
+        auto pDefaultShaderProgram = resourceManager.loadShaders(
+            "DefaultShader", "res/shaders/vertex.txt",
+            "res/shaders/fragment.txt");
         if (!pDefaultShaderProgram)
         {
-            std::cerr << "Can't create shader program: " << "DefaultShader" << std::endl;
+            std::cerr << "Can't create shader program: "
+                << "DefaultShader" << std::endl;
             return -1;
         }
 
-        auto pSpriteShaderProgram = resourceManager.loadShaders("SpriteShader", "res/shaders/vSprite.txt", "res/shaders/fSprite.txt");
+        auto pSpriteShaderProgram = resourceManager.loadShaders(
+            "SpriteShader", "res/shaders/vSprite.txt",
+            "res/shaders/fSprite.txt");
         if (!pSpriteShaderProgram)
         {
-            std::cerr << "Can't create shader program: " << "SpriteShader" << std::endl;
+            std::cerr << "Can't create shader program: "
+                << "SpriteShader" << std::endl;
             return -1;
         }
 
-        auto tex = resourceManager.loadTexture("DefaultTexture", "res/textures/pokemon.png");
+        auto tex = resourceManager.loadTexture("DefaultTexture",
+            "res/textures/emboar.png");
 
         std::vector<std::string> subTexturesNames = {
             "block",
@@ -114,8 +125,32 @@ int main(int argc, char** argv)
             "topRightBlock",
             "bottomLeftBlock",
             "bottomRightBlock",
-            "beton" };
-        auto pTextureAtlas = resourceManager.loatTextureAtlas("DefaultTextureAtlas", "res/textures/pokemon.png", std::move(subTexturesNames), 900, 900);
+            "beton", 
+
+            "pikachu",
+
+            "downemboarleft",
+            "downemboarright",
+            "downemboarmiddle",
+
+            "leftemboarleft",
+            "leftemboarright",
+            "leftemboarmiddle",
+
+            "rightmboarleft",
+            "rightemboarright",
+            "rightemboarmiddle",
+
+            "topemboarleft",
+            "topemboarright",
+            "topemboarmiddle",
+        };
+
+        auto pTextureAtlas = resourceManager.loatTextureAtlas(
+            "DefaultTextureAtlas",
+            "res/textures/emboar.png",
+            std::move(subTexturesNames),
+            860, 618);
 
         //auto pSprite = resourceManager.loadSprite("NewSprite", "DefaultTextureAtlas", "SpriteShader", 100, 100, "beton");
         //pSprite->setPosition(glm::vec2(300, 100));
@@ -123,17 +158,20 @@ int main(int argc, char** argv)
         GLuint points_vbo = 0;
         glGenBuffers(1, &points_vbo);
         glBindBuffer(GL_ARRAY_BUFFER, points_vbo);
-        glBufferData(GL_ARRAY_BUFFER, sizeof(point), point, GL_STATIC_DRAW);
+        glBufferData(GL_ARRAY_BUFFER,
+            sizeof(point), point, GL_STATIC_DRAW);
 
         GLuint colors_vbo = 0;
         glGenBuffers(1, &colors_vbo);
         glBindBuffer(GL_ARRAY_BUFFER, colors_vbo);
-        glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STATIC_DRAW);
+        glBufferData(GL_ARRAY_BUFFER,
+            sizeof(colors), colors, GL_STATIC_DRAW);
 
         GLuint texCoord_vbo = 0;
         glGenBuffers(1, &texCoord_vbo);
         glBindBuffer(GL_ARRAY_BUFFER, texCoord_vbo);
-        glBufferData(GL_ARRAY_BUFFER, sizeof(texCoord), texCoord, GL_STATIC_DRAW);
+        glBufferData(GL_ARRAY_BUFFER,
+            sizeof(texCoord), texCoord, GL_STATIC_DRAW);
 
         GLuint vao = 0;
         glGenVertexArrays(1, &vao);
@@ -141,32 +179,40 @@ int main(int argc, char** argv)
 
         glEnableVertexAttribArray(0);
         glBindBuffer(GL_ARRAY_BUFFER, points_vbo);
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+        glVertexAttribPointer(0,
+            3, GL_FLOAT, GL_FALSE, 0, nullptr);
 
         glEnableVertexAttribArray(1);
         glBindBuffer(GL_ARRAY_BUFFER, colors_vbo);
-        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+        glVertexAttribPointer(1,
+            3, GL_FLOAT, GL_FALSE, 0, nullptr);
 
         glEnableVertexAttribArray(2);
         glBindBuffer(GL_ARRAY_BUFFER, texCoord_vbo);
-        glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 0, nullptr);
+        glVertexAttribPointer(2,
+            2, GL_FLOAT, GL_FALSE, 0, nullptr);
 
         pDefaultShaderProgram->use();
         pDefaultShaderProgram->setInt("tex", 0);
 
-        glm::mat4 modelMatrix_1 = glm::mat4(1.f);
-        modelMatrix_1 = glm::translate(modelMatrix_1, glm::vec3(100.f, 50.f, 0.f));
+        //glm::mat4 modelMatrix_1 = glm::mat4(1.f);
+        //modelMatrix_1 = glm::translate(modelMatrix_1, glm::vec3(100.f, 50.f, 0.f));
+        //triangles with pikachu
+        //glm::mat4 modelMatrix_2 = glm::mat4(1.f);
+        //modelMatrix_2 = glm::translate(modelMatrix_2, glm::vec3(590.f, 50.f, 0.f));
 
-        glm::mat4 modelMatrix_2 = glm::mat4(1.f);
-        modelMatrix_2 = glm::translate(modelMatrix_2, glm::vec3(590.f, 50.f, 0.f));
+        glm::mat4 projectionMatrix = glm::ortho(0.f,
+            static_cast<float>(g_windowSize.x), 0.f,
+            static_cast<float>(g_windowSize.y),
+            -100.f, 100.f);
 
-        glm::mat4 projectionMatrix = glm::ortho(0.f, static_cast<float>(g_windowSize.x), 0.f, static_cast<float>(g_windowSize.y), -100.f, 100.f);
-
-        pDefaultShaderProgram->setMatrix4("projectionMat", projectionMatrix);
+        pDefaultShaderProgram->setMatrix4(
+            "projectionMat", projectionMatrix);
 
         pSpriteShaderProgram->use();
         pSpriteShaderProgram->setInt("tex", 0);
-        pSpriteShaderProgram->setMatrix4("projectionMat", projectionMatrix);
+        pSpriteShaderProgram->setMatrix4(
+            "projectionMat", projectionMatrix);
 
         /* Loop until the user closes the window */
         while (!glfwWindowShouldClose(pWindow))
@@ -178,11 +224,11 @@ int main(int argc, char** argv)
             glBindVertexArray(vao);
             tex->bind();
 
-            pDefaultShaderProgram->setMatrix4("modelMat", modelMatrix_1);
-            glDrawArrays(GL_TRIANGLES, 0, 3);
+            //pDefaultShaderProgram->setMatrix4("modelMat", modelMatrix_1);
+            //glDrawArrays(GL_TRIANGLES, 0, 3);
 
-            pDefaultShaderProgram->setMatrix4("modelMat", modelMatrix_2);
-            glDrawArrays(GL_TRIANGLES, 0, 3);
+            //pDefaultShaderProgram->setMatrix4("modelMat", modelMatrix_2);
+            //glDrawArrays(GL_TRIANGLES, 0, 3);
 
             //pSprite->render();
 
